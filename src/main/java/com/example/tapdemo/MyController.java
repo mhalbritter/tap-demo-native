144809af2a35f3383da0afd9e1416e991bddca35
Moritz Halbritter <mhalbritter@vmware.com>
2022-12-01 16:09:12 +0100

    Add logger

--- src/main/java/com/example/tapdemo/MyController.java
+++ src/main/java/com/example/tapdemo/MyController.java
@@ -1,5 +1,8 @@
 package com.example.tapdemo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,8 +11,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController("/")
 @RequestMapping(value = "/", produces = MediaType.TEXT_PLAIN_VALUE)
 public class MyController {
+	private final Logger logger = LoggerFactory.getLogger(getClass());
+
 	@GetMapping("/")
 	String index() {
+		logger.info("index()");
 		return "Hello, native TAP";
 	}
 }
