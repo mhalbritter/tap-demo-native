9dbfd9ecf80d3ca46852b5f460dcbefb0ab004a2
Moritz Halbritter <mhalbritter@vmware.com>
2022-12-01 16:04:55 +0100

    Import code

---
+++ src/main/java/com/example/tapdemo/TapDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.tapdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TapDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TapDemoApplication.class, args);
+	}
+
+}
